6d6ab71f66c279d02a8b1826b18ad783fafda83d
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2019-11-15 17:20:14 +0100

    moded code from pso_method.py to a separate package

---
+++ methods/DPSO/DPSO.py
@@ -0,0 +1,9 @@
+class DPSO:
+    def __init__(self, pop_size, coef_inertia, coef_personal, coef_social):
+        self.pop_size = pop_size
+        self.coef_inertia=coef_inertia
+        self.coef_personal=coef_personal
+        self.coef_social=coef_social
+
+    def optimize(self):
+        pass
